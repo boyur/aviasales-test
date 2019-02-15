47c48aa104788a48c1c78dcfce3a4216a022f949
boyur <gboyur@gmail.com>
2019-02-15 21:00:52 +0300

    Updates

---
+++ precache-manifest.c16beb268106cb3244b28d9af925637c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55ea38685ffdfa89e3aa",
+    "url": "/aviasales-test/static/js/runtime~main.55ea3868.js"
+  },
+  {
+    "revision": "fbd809aa32532b4704de",
+    "url": "/aviasales-test/static/js/main.fbd809aa.chunk.js"
+  },
+  {
+    "revision": "39cac40ff9720975b1d8",
+    "url": "/aviasales-test/static/js/2.39cac40f.chunk.js"
+  },
+  {
+    "revision": "fbd809aa32532b4704de",
+    "url": "/aviasales-test/static/css/main.531a9f59.chunk.css"
+  },
+  {
+    "revision": "39cac40ff9720975b1d8",
+    "url": "/aviasales-test/static/css/2.b712b032.chunk.css"
+  },
+  {
+    "revision": "b7861db8fe7cd7de06eeb23d77da1179",
+    "url": "/aviasales-test/index.html"
+  }
+];
