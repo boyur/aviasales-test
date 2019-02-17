31c8df7c8e722c768369305061a201f4e34c1f57
boyur <gboyur@gmail.com>
2019-02-17 14:08:23 +0300

    pixel perfect

--- src/components/SettingsPanel/CurrencyToggle/CurrencyToggle.js
+++ src/components/SettingsPanel/CurrencyToggle/CurrencyToggle.js
@@ -5,25 +5,27 @@ import PropTypes from 'prop-types';
 const currencies = ['RUB', 'USD', 'EUR'];
 
 const CurrencyToggle = ({ currency, handleChangeCurrency }) => (
-  <div className="currency-toggle">
-    {
-      currencies.map((value) => {
-        const isActive = value === currency;
-        const className = isActive ? 'currency-toggle__button is-active' : 'currency-toggle__button';
-        const onClick = () => handleChangeCurrency(value);
+  <div className="currency-toggle__wrapper">
+    <div className="currency-toggle">
+      {
+        currencies.map((value) => {
+          const isActive = value === currency;
+          const className = isActive ? 'currency-toggle__button is-active' : 'currency-toggle__button';
+          const onClick = () => handleChangeCurrency(value);
 
-        return (
-          <button
-            key={value}
-            className={className}
-            onClick={onClick}
-            type="button"
-          >
-            {value}
-          </button>
-        );
-      })
-    }
+          return (
+            <button
+              key={value}
+              className={className}
+              onClick={onClick}
+              type="button"
+            >
+              {value}
+            </button>
+          );
+        })
+      }
+    </div>
   </div>
 );
 
