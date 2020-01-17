3eeb9b1b130da8a56bcd5446230cc2bfb147b94a
Artem Boyur <gboyur@gmail.com>
2020-01-17 14:16:45 +0300

    Update urls.js

--- src/configs/urls.js
+++ src/configs/urls.js
@@ -1,4 +1,4 @@
 export default {
-  tickets: 'https://raw.githubusercontent.com/KosyanMedia/test-tasks/master/aviasales/tickets.json',
+  tickets: 'https://raw.githubusercontent.com/KosyanMedia/test-tasks/master/DEPRECATED_aviasales/tickets.json',
   exchangeRates: 'https://openexchangerates.org/api/latest.json?app_id=0b6a22c769734cd388b5b0bee2946fc6&currencies=RUB,EUR',
 };
