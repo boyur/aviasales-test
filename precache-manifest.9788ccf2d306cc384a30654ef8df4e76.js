b0fa618ab991aa2a7474b997cdac6398aead5adf
Artem Boyur <boyur@yandex-team.ru>
2020-01-17 14:21:49 +0300

    Updates

---
+++ precache-manifest.9788ccf2d306cc384a30654ef8df4e76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "afa0eb8404bd966e47a8c7c8771774f5",
+    "url": "/aviasales-test/static/media/checkMark.afa0eb84.svg"
+  },
+  {
+    "revision": "bb607214ab731f3aa1bfa2200e1d8df0",
+    "url": "/aviasales-test/static/media/airplane.bb607214.svg"
+  },
+  {
+    "revision": "db0a23102b6232719ad2a5b8f6252769",
+    "url": "/aviasales-test/static/media/Logo.db0a2310.svg"
+  },
+  {
+    "revision": "3e9ea761ec2ab649ea73",
+    "url": "/aviasales-test/static/js/runtime~main.3e9ea761.js"
+  },
+  {
+    "revision": "c9f90bb95cb908b97c64",
+    "url": "/aviasales-test/static/js/main.c9f90bb9.chunk.js"
+  },
+  {
+    "revision": "e9c58d880ddc3ac9c957",
+    "url": "/aviasales-test/static/js/2.e9c58d88.chunk.js"
+  },
+  {
+    "revision": "c9f90bb95cb908b97c64",
+    "url": "/aviasales-test/static/css/main.28128d05.chunk.css"
+  },
+  {
+    "revision": "e9c58d880ddc3ac9c957",
+    "url": "/aviasales-test/static/css/2.021a9d49.chunk.css"
+  },
+  {
+    "revision": "d71ab5fec156935298c2fdcb347538c4",
+    "url": "/aviasales-test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/aviasales-test/precache-manifest.ce8977194175c5b03eaaf2c43c85ba04.js"
+  "/aviasales-test/precache-manifest.9788ccf2d306cc384a30654ef8df4e76.js"
 );
 
 workbox.clientsClaim();
